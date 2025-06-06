93e8018b298aa8ac1044568c85d484e805511087
oneday-lzs <15322386667@163.com>
2025-06-06 22:42:28 +0800

    feat(dir): [components] el-table

---
+++ packages/components/el-table/style/css.ts
@@ -0,0 +1,5 @@
+import '@smart-table/components/base/style/css';
+import '@smart-table/theme-chalk/el-table.css';
+import '@smart-table/components/checkbox/style/css';
+import '@smart-table/components/tooltip/style/css';
+import '@smart-table/components/scrollbar/style/css';
---
+++ packages/components/el-table/style/index.ts
@@ -0,0 +1,5 @@
+import '@smart-table/components/base/style';
+import '@smart-table/theme-chalk/src/table.scss';
+import '@smart-table/components/checkbox/style';
+import '@smart-table/components/tooltip/style';
+import '@smart-table/components/scrollbar/style';
